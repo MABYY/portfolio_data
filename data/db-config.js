54852ea707a64ac3d7a7923db4b34591e77dad27
Maria Negri <mariamnegri@gmail.com>
2022-06-12 17:23:49 -0300

    Knex file update

--- data/db-config.js
+++ data/db-config.js
@@ -1,5 +1,5 @@
 var configs = require('../knexfile')
 var knex = require('knex')
-var environment = process.env.NODE_ENV 
-module.exports = knex(configs[environment]) 
+var env = process.env.NODE_ENV 
+module.exports = knex(configs[env]) 
 
--- knexfile.js
+++ knexfile.js
@@ -2,10 +2,44 @@ require('dotenv').config()
 const pg = require('pg')
 
 if (process.env.DATABASE_URL) {
-  pg.defaults.ssl = { rejectUnauthorized: false }
+    pg.defaults.ssl = { rejectUnauthorized: false }
+}
+
+const sharedConfig = {
+    client: 'pg',
+    migrations: { directory: './migrations' },
+    seeds: { directory: './seeds' },
 }
 
 module.exports = {
+    development: {
+        ...sharedConfig,
+        connection: process.env.DEV_DATABASE_URL,
+    },
+    test: {
+        ...sharedConfig,
+        connection: process.env.TESTING_DATABASE_URL,
+    },
+    production: {
+        ...sharedConfig,
+        connection: process.env.DATABASE_URL,
+        pool: { min: 2, max: 10 },
+    },
+}
+
+
+
+
+
+
+// require('dotenv').config()
+// const pg = require('pg')
+
+// if (process.env.DATABASE_URL) {
+//   pg.defaults.ssl = { rejectUnauthorized: false }
+// }
+
+// module.exports = {
   //  testing : {
   //   client : 'sqlite3',
   //   connection: {
@@ -20,35 +54,35 @@ module.exports = {
   
   //   },
   // },
-  testing : {
-    client : 'pg',
-    connection:  process.env.DATABASE_URL,
-    pool: { min: 2 , max: 10 },
+  // testing : {
+  //   client : 'pg',
+  //   connection:  process.env.DATABASE_URL,
+  //   pool: { min: 2 , max: 10 },
 
-    migrations: {
-        directory: './migrations'
-    },
-    seeds :{
-        directory:'./seeds'
+  //   migrations: {
+  //       directory: './migrations'
+  //   },
+  //   seeds :{
+  //       directory:'./seeds'
   
-    },
+  //   },
     
-  },
-  development : {
-    client : 'pg',
-    connection:  process.env.DATABASE_URL,
-    pool: { min: 2 , max: 10 },
+  // },
+//   development : {
+//     client : 'pg',
+//     connection:  process.env.DATABASE_URL,
+//     pool: { min: 2 , max: 10 },
 
-    migrations: {
-        directory: './migrations'
-    },
-    seeds :{
-        directory:'./seeds'
+//     migrations: {
+//         directory: './migrations'
+//     },
+//     seeds :{
+//         directory:'./seeds'
   
-    },
+//     },
     
-  },
-}
+//   },
+// }
 // https://stackoverflow.com/questions/54302088/how-to-fix-error-the-server-does-not-support-ssl-connections-when-trying-to-a
 // const pg = require('pg')
 
